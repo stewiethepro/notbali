dccd255e6597f0bd04289349e60dd40b70abb13b
“stewiethepro” <petersloan1984@gmail.com>
2024-09-11 19:11:14 +1200

    Itinerary changes

--- components/landing/itinerary-section.tsx
+++ components/landing/itinerary-section.tsx
@@ -104,7 +104,7 @@ const cardData: CardDataProps[] = [
       id: 3,
       title: "December 11th",
       content:
-        "Fly to Queenstown babyyy",
+        "Fly to Queenstown babyyy!",
       image:
         "https://images.unsplash.com/photo-1720378042271-60aff1e1c538?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D",
     },
